3a3a58b6599ce8cb3f1565e720e025bb62f0ddb3
Filip Wądołowski <84192488+fkwadolowski@users.noreply.github.com>
2024-06-26 16:23:30 +0200

    jinja flask and api combination simple project

--- server.py
+++ server.py
@@ -1,9 +1,28 @@
-from flask import Flask,render_template
-app=Flask(__name__)
+import time
+from flask import Flask, render_template
+import requests
+
+date = time.strftime('%Y')
+YOUR_NAME="Filip Wadolowski"
+app = Flask(__name__)
+
 
 @app.route('/')
 def home():
-    return render_template("index.html", name="hello world!")
+    return render_template("index.html", name="hello world!",
+                           CURRENT_YEAR=date,
+                           YOUR_NAME=YOUR_NAME)
 
-if __name__=="__main__":
+
+@app.route('/guess/<some_name>')
+def geuss(some_name):
+    gender_req = requests.get(url=f"https://api.genderize.io?name={some_name}")
+    gender = gender_req.json()["gender"]
+    age_req = requests.get(url=f"https://api.agify.io?name={some_name}")
+    age = age_req.json()["age"]
+    return render_template("index.html", name=some_name.title(),
+                           gender=gender, age=age, YOUR_NAME=YOUR_NAME)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
